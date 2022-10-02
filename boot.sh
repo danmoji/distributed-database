da66da5fcfd7576a9239875bf0f24ce4f3d735e3
danjogin <beberosak@gmail.com>
2022-10-02 22:40:31 +0200

    merge network setup and boot script

--- boot.sh
+++ boot.sh
@@ -1,3 +1,4 @@
+docker network create backend-network
 docker kill $(docker ps -q)
 cd sql-a
 docker-compose up -d
@@ -8,3 +9,6 @@ cd ..
 cd sql-c
 docker-compose up -d
 cd ..
+docker network connect backend-network sql-a_web_1
+docker network connect backend-network sql-b_web_1
+docker network connect backend-network sql-c_web_1
--- network-setup.sh
+++
@@ -1,4 +0,0 @@
-docker network create backend-network
-docker network connect backend-network sql-a_web_1
-docker network connect backend-network sql-b_web_1
-docker network connect backend-network sql-c_web_1
