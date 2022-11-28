58c230a20eac45981f0c6fede10f8cda531b35a9
danjogin <beberosak@gmail.com>
2022-11-28 08:57:11 +0100

    modify boot

--- boot.sh
+++ boot.sh
@@ -1,10 +1,12 @@
-docker kill $(docker ps -q)
 cd sql-a
-docker-compose up -d
+docker-compose down
+docker-compose up -d 
 cd ..
 cd sql-b
-docker-compose up -d
+docker-compose down
+docker-compose up -d 
 cd ..
 cd sql-c
-docker-compose up -d
+docker-compose down
+docker-compose up -d 
 cd ..
